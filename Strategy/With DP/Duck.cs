2732c039129c9a01512011944da02ecfb3bff733
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-08-03 18:06:06 +0300

    Strategy Design Pattern

---
+++ Strategy/With DP/Duck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.With_DP
+{
+    internal abstract class Duck
+    {
+        protected Duck(IFlyBehaviour flyBehaviour, IQuackBehaviour quackBehaviour)
+        {
+            FlyBehaviour = flyBehaviour;
+            QuackBehaviour = quackBehaviour;
+        }
+
+        public abstract void Display();
+
+        public void Swim()
+            => Console.WriteLine("I am Swimming");
+
+        public IFlyBehaviour FlyBehaviour { get; set; }
+        public IQuackBehaviour QuackBehaviour { get; set; }
+
+        public void Fly(Action FlyAction)
+        {
+            FlyAction.Invoke();
+        }
+        public void Quack()
+        {
+            QuackBehaviour.PerformQuack();
+        }
+    }
+}
---
+++ Strategy/With No DP/MallardDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.With_No_DP
+{
+    internal class MallardDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I am Mallerd Duck *_*");
+        }
+    }
+}
